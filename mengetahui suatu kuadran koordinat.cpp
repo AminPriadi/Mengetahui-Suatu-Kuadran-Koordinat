4a3940d7bf6393aa04f63f6e011359d12eaa1c58
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 19:04:24 +0700

    Add files via upload

---
+++ mengetahui suatu kuadran koordinat.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"================================="<<endl;
+    cout<<"=MENCARI KUADRAN SUATU KOORDINAT="<<endl;
+    cout<<"================================="<<endl;
+    cout<<"masukan koordinat x\n";
+    cin>>x;
+    cout<<"masukan koordinat y\n";
+    cin>>y;
+    {
+        if(x>=0 && y>=0)
+            cout<<"berada dikuadran I "<<endl;
+        if(x<=0 && y>=0)
+            cout<<"berada dikuadran II "<<endl;
+        if(x<=0 && y<=0)
+            cout<<"berada dikuadran III "<<endl;
+        if(x>=0 && y<=0)
+            cout<<"berada dikuadran IV "<<endl;
+    }
+    return 0;
+}
